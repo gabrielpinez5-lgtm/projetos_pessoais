c183cab455faa66f5a3280620535309324ef91ac
Gabriel Pinez <gabrielpinez5@gmail.com>
2026-03-25 12:11:09 -0300

    divisores logicos

--- teste_divisao/divisao.py
+++ teste_divisao/divisao.py
@@ -1,9 +1,51 @@
-numero = int(input("insira um valor"))
-contagem = 0
-divisores = []
-for i in range(numero):
-    contagem += 1
-    if numero % contagem ==  0:
-        divisores.append(contagem)
+import subprocess
 
-print(f"\nseus divisores sao:{divisores}")
+
+#def seletor():
+#    print("digite 0 para sair e 1 para descobrir os divisores de um numero")
+#    opcao_escolhida = int(input("digite aqui\t"))
+#    return opcao_escolhida
+
+
+
+def divisao():
+    numero = -1
+    while numero != 0:
+        print("insira um numero  ou digite 0 se desejar sair da repetição\n")
+        numero = int(input("insira um valor natural não nulo:\t"))
+        if numero != 0:
+            if numero > 0:
+                subprocess.run('cls', shell=True)
+                contagem = 0
+                divisores = []
+                for i in range(numero):
+                    contagem += 1
+                    if numero % contagem ==  0:
+                        divisores.append(contagem)
+
+                if len(divisores) == 2:
+                    print(f"numero primo, seus divisores são 1 e {numero}")
+                else:
+                    print(f"\nos divisores de {numero} sao:{divisores}")
+                input("pressione [enter] para continuar")
+            else:
+                print("\n\nINSIRA UM NUMERO NATURAL NAO NULO\n\n\n")
+    print("\n\nsaindo...")
+    print("ate breve!")
+
+
+#def principal():
+#    opcao = -1
+#    while opcao != 0:
+#        opcao = seletor()
+#        if opcao != 0:
+#            match opcao:
+#                case 1: divisao()
+#                case _:
+#                    print("digite uma oção valida")
+#                    input("pressione [enter] para continuar")
+
+if __name__ == "__main__":
+    subprocess.run('cls', shell=True)
+#    principal()
+    divisao()
