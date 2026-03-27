99e1a7dba1dc7d19d814a83531fe363ecd7c5ea1
gabrielpinez5-lgtm <gabrielpinez5@gmail.com>
2026-03-26 22:53:35 -0300

    aprendendo pra prova do chico

--- aprendendo_ler_arquivo/arq.py
+++ aprendendo_ler_arquivo/arq.py
@@ -1,17 +1,52 @@
+import subprocess
+
 class ARQUIVO():
     def __init__(self) -> None:
         self._soma = 0.0
         self._soma_valor_com_peso = 0.0
         self._soma_peso =0.0
         self._quantos_numero_somados = 0
+        self._soma_inversos = 0.0
+        self._produto_valores = 1
+        self._soma_de_quadrados = 0.0
 
 
 
     def receber_arquivo(self, valor, peso):
         self._soma += valor
+        self._soma_inversos += 1 / valor
+        self._produto_valores *= valor
         self._soma_peso += peso
-        self._soma_valor_com_peso = valor * peso
+        self._soma_valor_com_peso += valor * peso
         self._quantos_numero_somados += 1
+        self._soma_de_quadrados += valor ** 2
+
+    def limpar_visor(self):
+        subprocess.run('cls', shell=True)
+        self._soma = 0.0
+        self._soma_valor_com_peso = 0.0
+        self._soma_peso =0.0
+        self._quantos_numero_somados = 0
+        self._soma_inversos = 0.0
+        self._produto_valores = 1
+
+
+    @property
 
     def media_ponderada(self):
-        pass
+        return self._soma_valor_com_peso / self._soma_peso
+    
+    @property
+
+    def rmq(self):
+        return (self._soma_de_quadrados / self._quantos_numero_somados) ** 0.5
+    
+    @property
+
+    def media_harmonica(self):
+        return self._quantos_numero_somados /  self._soma_inversos
+    
+    @property
+    
+    def media_geometrica(self):
+        return self._produto_valores ** (1 / self._quantos_numero_somados)
--- aprendendo_ler_arquivo/main.py
+++ aprendendo_ler_arquivo/main.py
@@ -1,9 +1,29 @@
+from arq import ARQUIVO
+import subprocess 
+
+
 def principal():
+    leitura = ARQUIVO()
     arquivo = str(input("Insira o caminho do arquivo.txt a ser lido:\n\n"))
     arquivo = open(arquivo)
-    linha = arquivo.readline()
+    linha = "-"
+    
     while linha != "":
-        pass
+        linha = arquivo.readline()
+        if linha != "":
+            valor_str, peso_str = linha.strip().split(';')
+            valor_float = float(valor_str)
+            peso_float = float(peso_str)
+            leitura.receber_arquivo(valor_float, peso_float)
+    subprocess.run('cls', shell=True)
+    print(f"a media ponderada é aproximadamente {leitura.media_ponderada}")
+    print(f"a media harmonica é aproximadamente {leitura.media_harmonica}")
+    print(f"a media geometrica é aproximadamente {leitura.media_geometrica}")
+    print(f"a raiz media quadratica é aproximadamente {leitura.rmq}")
+    input("pressione [enter] para continuar!")
+    
+
+
 
 if __name__ == "__main__":
     principal()
